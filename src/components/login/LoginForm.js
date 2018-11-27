c3f44ad56e40e4534b89aa5e09edb5dc62e9b9b6
DavidB59 <david.behal@net-c.com>
2018-11-27 16:54:47 +0100

    initial commit

---
+++ src/components/login/LoginForm.js
@@ -0,0 +1,43 @@
+import React, {PureComponent} from 'react'
+import './LoginForm.css'
+
+export default class LoginForm extends PureComponent {
+	state = {}
+
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.props.onSubmit(this.state)
+	}
+
+	handleChange = (event) => {
+    const {name, value} = event.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+	render() {
+		return (
+      <div className="login-form">
+  			<form onSubmit={this.handleSubmit}>
+  				<label>
+            Email
+            <input type="email" name="email" value={
+  						this.state.email || ''
+  					} onChange={ this.handleChange } />
+          </label>
+
+  				<label>
+            Password
+            <input type="password" name="password" value={
+  						this.state.password || ''
+  					} onChange={ this.handleChange } />
+          </label>
+
+  				<button type="submit">Login</button>
+					 <img src="https://deckofcardsapi.com/static/img/7C.png" alt='an image' ></img>
+  			</form>
+		  </div>)
+	}
+}
