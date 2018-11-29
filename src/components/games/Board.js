577c3e43715a73876f7f5b0b516494b6d8cfbeca
DavidB59 <david.behal@net-c.com>
2018-11-29 14:57:37 +0100

    correctly positioned

--- src/components/games/Board.js
+++ src/components/games/Board.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Board.css'
-import {backOfCard} from '../../constants'
+import {backOfCard, tapisCarte} from '../../constants'
 
 const renderCel = (makeMove, rowIndex, cellIndex, symbol, hasTurn) => {
   return (
@@ -21,18 +21,28 @@ const renderCel = (makeMove, rowIndex, cellIndex, symbol, hasTurn) => {
 
 export default function Board (props ){
   return (<div>
-    {props.handPlayer1.hand.map( card => <img className="visibleCard"
-src={card.image} alt={card.code}/> )}
 
-    {//for (let i=0, i< props.handPlayer1.hand.length,i++) {
-     props.handPlayer2.hand.map( card => 
-      <img className="visibleCard"  src={backOfCard} alt="backOfCard"/> 
+ {      <img style={{"position" :"relative","height":" 800px"}}   src={tapisCarte} alt={props.deck[0].code}/> 
+}
+    {props.handPlayer1.hand.map( (card,vanyaRocks) =>
+    <img   style={{"position" :"absolute", "top":"910px", "left":`${vanyaRocks*80+450}px`, "height":" 150px"}}
+     src={card.image} alt={card.code}/> 
+    )}
+  
+    {
+     props.handPlayer2.hand.map((card, vanyaRocks) =>      
+      <img  style={{"position" :"absolute", "top":"240px", "left":`${vanyaRocks*80+400}px`, "height":" 150px"}}
+
+       src={backOfCard} alt="backOfCard"/> 
+
       )
     }
-
-    {      <img className="trumpCard"  src={props.deck[0].image} alt={props.deck[0].code}/> 
+    {      <img style={{"position" :"absolute", "top":"570px", "left":`240px`, "height":" 150px"}}
+     src={props.deck[0].image} alt={props.deck[0].code}/> 
 }
+
+
+
   </div>)}
 
-
-  // `eHIS IS A CARD ${card.code}` 
+  
